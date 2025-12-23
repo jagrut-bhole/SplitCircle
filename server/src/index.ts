1a6f2ea02dd880bcb999200c6d5b3790c3e9ab72
Jagrut Bhole <jagrutbhole75@gmail.com>
2025-12-23 17:23:50 +0530

    configure heroku deployment for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 export const prisma = new PrismaClient({ adapter });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
