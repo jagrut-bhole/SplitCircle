107c21d78c39d568c0933fe2b17c36d55fb87aaf
Jagrut Bhole <jagrutbhole75@gmail.com>
2025-12-23 18:03:48 +0530

    minor change

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -9,7 +9,7 @@ export const ProtectedRouter = ({children} : ProtectedRouteProps) => {
     const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
 
     if (!isAuthenticated) {
-        return <Navigate to='/login' replace />
+        return <Navigate to='/' replace />
     }
 
     return <>{children}</>
